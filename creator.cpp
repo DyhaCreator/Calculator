138cd669380b4f66aabb1bd6559196d1f14918a6
DyhaCreator <andropet228@gmail.com>
2023-11-22 00:55:22 +0500

    faster

--- creator.cpp
+++ creator.cpp
@@ -3,6 +3,7 @@
 
 int main() {
     std::ofstream out("my_calculator.py");
+    out << "print('Hello and welcome to my calculator')" << std::endl;
     out << "a = input().split()" << std::endl;
     int first_min = -127;
     int first_max = 127;
@@ -10,25 +11,25 @@ int main() {
     int second_max = 127;
     for (int i = first_min; i <= first_max; i++) {
         for (int j = second_min; j <= second_max; j++) {
-            out << "if int(a[0]) == " << i << " and a[1] == '+' and int(a[2]) == " << j << ":" << std::endl;
+            out << "if a[0] == '" << i << "' and a[1] == '+' and a[2] == '" << j << "':" << std::endl;
             out << "    print(" << i + j << ")" << std::endl;
         }
     }
     for (int i = first_min; i <= first_max; i++) {
         for (int j = second_min; j <= second_max; j++) {
-            out << "if int(a[0]) == " << i << " and a[1] == '-' and int(a[2]) == " << j << ":" << std::endl;
+            out << "if a[0] == '" << i << "' and a[1] == '-' and a[2] == '" << j << "':" << std::endl;
             out << "    print(" << i - j << ")" << std::endl;
         }
     }
     for (int i = first_min; i <= first_max; i++) {
         for (int j = second_min; j <= second_max; j++) {
-            out << "if int(a[0]) == " << i << " and a[1] == '*' and int(a[2]) == " << j << ":" << std::endl;
+            out << "if a[0] == '" << i << "' and a[1] == '*' and a[2] == '" << j << "':" << std::endl;
             out << "    print(" << i * j << ")" << std::endl;
         }
     }
     for (int i = first_min; i <= first_max; i++) {
         for (int j = second_min; j <= second_max; j++) {
-            out << "if int(a[0]) == " << i << " and a[1] == '/' and int(a[2]) == " << j << ":" << std::endl;
+            out << "if a[0] == '" << i << "' and a[1] == '/' and a[2] == '" << j << "':" << std::endl;
             out << "    print('" << (float)i / (float)j << "')" << std::endl;
         }
     }
