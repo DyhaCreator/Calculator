c69e15d1a447041d06cb4bae38838642cac46670
DyhaCreator <andropet228@gmail.com>
2023-11-22 00:38:33 +0500

    initial Commit

--- creator.cpp
+++ creator.cpp
@@ -5,9 +5,9 @@ int main() {
     std::ofstream out("my_calculator.py");
     out << "a = input().split()" << std::endl;
     int first_min = -127;
-    int first_max = 128;
+    int first_max = 127;
     int second_min = -127;
-    int second_max = 128;
+    int second_max = 127;
     for (int i = first_min; i <= first_max; i++) {
         for (int j = second_min; j <= second_max; j++) {
             out << "if int(a[0]) == " << i << " and a[1] == '+' and int(a[2]) == " << j << ":" << std::endl;
