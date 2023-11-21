2eb63584d3841f42892357d74e1c6573c94a219c
DyhaCreator <andropet228@gmail.com>
2023-11-22 00:37:45 +0500

    new

---
+++ creator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::ofstream out("my_calculator.py");
+    out << "a = input().split()" << std::endl;
+    int first_min = -127;
+    int first_max = 128;
+    int second_min = -127;
+    int second_max = 128;
+    for (int i = first_min; i <= first_max; i++) {
+        for (int j = second_min; j <= second_max; j++) {
+            out << "if int(a[0]) == " << i << " and a[1] == '+' and int(a[2]) == " << j << ":" << std::endl;
+            out << "    print(" << i + j << ")" << std::endl;
+        }
+    }
+    return 0;
+}
