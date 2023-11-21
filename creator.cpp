c03b2d95fe440bc24fef7035956ee79f51b9917d
DyhaCreator <andropet228@gmail.com>
2023-11-22 01:00:45 +0500

    add sqrt

--- creator.cpp
+++ creator.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 int main() {
     std::ofstream out("my_calculator.py");
     out << "print('Hello and welcome to my calculator')" << std::endl;
     out << "a = input().split()" << std::endl;
-    int first_min = -127;
-    int first_max = 127;
-    int second_min = -127;
-    int second_max = 127;
-    for (int i = first_min; i <= first_max; i++) {
-        for (int j = second_min; j <= second_max; j++) {
+    out << "a.append(-1)" << std::endl;
+    int min = -127;
+    int max = 127;
+    for (int i = min; i <= max; i++) {
+        for (int j = min; j <= max; j++) {
             out << "if a[0] == '" << i << "' and a[1] == '+' and a[2] == '" << j << "':" << std::endl;
             out << "    print(" << i + j << ")" << std::endl;
         }
     }
-    for (int i = first_min; i <= first_max; i++) {
-        for (int j = second_min; j <= second_max; j++) {
+    for (int i = min; i <= max; i++) {
+        for (int j = min; j <= max; j++) {
             out << "if a[0] == '" << i << "' and a[1] == '-' and a[2] == '" << j << "':" << std::endl;
             out << "    print(" << i - j << ")" << std::endl;
         }
     }
-    for (int i = first_min; i <= first_max; i++) {
-        for (int j = second_min; j <= second_max; j++) {
+    for (int i = min; i <= max; i++) {
+        for (int j = min; j <= max; j++) {
             out << "if a[0] == '" << i << "' and a[1] == '*' and a[2] == '" << j << "':" << std::endl;
             out << "    print(" << i * j << ")" << std::endl;
         }
     }
-    for (int i = first_min; i <= first_max; i++) {
-        for (int j = second_min; j <= second_max; j++) {
+    for (int i = min; i <= max; i++) {
+        for (int j = min; j <= max; j++) {
             out << "if a[0] == '" << i << "' and a[1] == '/' and a[2] == '" << j << "':" << std::endl;
             out << "    print('" << (float)i / (float)j << "')" << std::endl;
         }
+    }
+    for (int i = 0; i < max; i++) {
+        out << "if a[0] == 'sqrt(" << i << ")':" << std::endl;
+        out << "    print(" << std::sqrt(i) << ")" << std::endl; 
     }
     return 0;
 }
